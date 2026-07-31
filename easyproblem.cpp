88a61f853ef35a38a7d906b180d557463b317e69
harsimrankaur17 <h84933981@gmail.com>
2026-07-31 20:23:35 +0530

    Create easyproblem.cpp

---
+++ easyproblem.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for(int i=1; i<=t; i++){
+        int x;
+        cin>>x;
+        cout<<x-1<<endl;
+        
+    }
+    
+    return 0;
+}
