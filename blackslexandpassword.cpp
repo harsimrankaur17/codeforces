28d19a699cb868165f2a16ca3ce91cd3c4042197
harsimrankaur17 <h84933981@gmail.com>
2026-08-02 19:26:06 +0530

    Create blackslexandpassword.cpp

---
+++ blackslexandpassword.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for(int i=1; i<=t; i++){
+        
+        int x;
+        cin>>x;
+        int y;
+        cin>>y;
+        cout<<x*y +1<<endl;
+    }
+    
+    
+    return 0;
+}
