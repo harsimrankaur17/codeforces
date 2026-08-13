362b02d86f3a817cd1b02c0e9b48b74d2c92e3e1
harsimrankaur17 <h84933981@gmail.com>
2026-08-13 21:17:18 +0530

    Add div 2 contest solution in C++

---
+++ div 2 contest.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int arr[3];
+        for (int i = 0; i < 3; i++)
+        {
+            cin >> arr[i];
+        }
+        sort(arr, arr + 3);
+        if (arr[0] + arr[1] <= arr[2])
+        {
+            cout << arr[1] << endl;
+        }
+        else
+        {
+            cout << arr[2] - arr[0] << endl;
+        }
+    }
+    return 0;
+}
