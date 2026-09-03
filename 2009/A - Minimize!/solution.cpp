058695a3239aa1031b35e43e7f671331dc4b095f
harsimrankaur17 <h84933981@gmail.com>
2026-09-03 21:13:50 +0530

    Add Minimize! [A] from Codeforces

---
+++ 2009/A - Minimize!/solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){ int x;
+cin>>x;
+while(x--){
+    int a,b;
+    cin>>a>>b;
+    cout<<b-a<<endl;
+    
+    
+    
+    
+}
+    
+    
+    
+    return 0;
+}
